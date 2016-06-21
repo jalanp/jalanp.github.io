8d031bd07685291e5882b66ed2927e0e4f1f2556
pjalan01 <Prajvin.Jalan@teranet.ca>
2016-06-21 11:47:21 -0400

    mobile nav test on contact page

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,5 @@
 $(document).ready(function(){
+	var displayNav = 0;
 	
 	/*-----change logo image links on hover and unhover-----*/
 	$("#img-github").hover(function(){
@@ -37,7 +38,21 @@ $(document).ready(function(){
 	
 	/*-----resize the content to match the header accordingly-----*/
 	$("#main-content-wrapper").mainContentResize();
+	headerResize(displayNav);
 	$(window).resize(function(){
+		//display the navigation buttons if not mobile screen sizes
+		if (!(window.matchMedia("(max-width: 414px)").matches) && 
+			!(window.matchMedia("(max-width: 580px)").matches)){
+			$("#nav").css("display","inline-block");
+		}
+		else {$("#nav").css("display","none");} //otherwise hide navigation buttons on small screen resize
+		
+		//assign displayNav a value based on if navigation is being displayed
+		if ($("#nav").css("display") == "none"){displayNav = 0;}
+		else {displayNav = 1;}
+		
+		//resize header and page content
+		headerResize(displayNav);
 		$("#main-content-wrapper").mainContentResize();
 	});
 	
@@ -57,8 +72,53 @@ $(document).ready(function(){
 		var fullText = words.join(" ");
 		$(this).html(fullText);
 	});
+
+	/*-----to remove logo icons when screen is smaller-----*/
+	checkGitlink();
+	$(window).resize(function(){
+		checkGitlink();
+	});
+	
+	/*-----mobile navigation menu button click-----*/
+	$("#mobile-nav-button").click(function(){
+		if ($("#nav").css("display") == "none"){
+			$("#nav").css("display","inline-block");
+			displayNav = 1;
+		}
+		else {
+			$("#nav").css("display","none");
+			displayNav = 0;
+		}
+		
+		//resize header and page content
+		headerResize(displayNav);
+		$("#main-content-wrapper").mainContentResize();
+	});
 });
 
+//resize content based on header
 $.fn.mainContentResize = function(){
 	$(this).css("top",$("header").height());
+}
+
+//change display for logo icons
+function checkGitlink() {
+	if (window.matchMedia("(max-width: 580px)").matches){$(".gitlink").css("display","none");}
+	else {$(".gitlink").css("display","inline-block");}
+}
+
+//resize header based on max-width of screen
+function headerResize(displayNav) {
+	if (displayNav == 1) {
+		if (window.matchMedia("(max-width: 414px)").matches){$("header").css("height","170px");}
+		else if (window.matchMedia("(max-width: 580px)").matches){$("header").css("height","120px");}
+		else if (window.matchMedia("(max-width: 1050px)").matches){$("header").css("height","150px");}
+		else {$("header").css("height","100px");}
+	}
+	else {
+		if (window.matchMedia("(max-width: 414px)").matches){$("header").css("height","60px");}
+		else if (window.matchMedia("(max-width: 580px)").matches){$("header").css("height","60px");}
+		else if (window.matchMedia("(max-width: 1050px)").matches){$("header").css("height","150px");}
+		else {$("header").css("height","100px");}
+	}
 }
